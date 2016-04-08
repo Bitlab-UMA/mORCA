3fe8fec69931a1bd20c18774b1f434f834973403
juanfal <juan.falgueras@gmail.com>
2016-04-08 17:02:24 +0200

    mainresults popupfiles dissappear

--- js/sAPI.js
+++ js/sAPI.js
@@ -206,9 +206,9 @@ function generateInterface(parameters) {
 
           for (var y in filesList) {
             document.write(
-              '<li><a onclick="fillText(' + x + ',\'' + filesList[y].id + '\');">' + filesList[y].name + '</a></li>'
+              '<li><a onclick="fillText(' + x + ',\'' + filesList[y].id + '\'); $("#popupMenu"+x+"-popup").popup('close');">' + filesList[y].name + '</a></li>'
             );
-            $("#popupMenu"+x+"-popup").popup('close');
+
           }
         }
 
