fa2423ecc27134bd351fa59746b20943642319ad
juanfal <juan.falgueras@gmail.com>
2016-04-28 14:56:07 +0200

    selecting results

--- js/mAPI.js
+++ js/mAPI.js
@@ -412,7 +412,7 @@ alert(clData);
             'charsPerLine': 60,
             'toolbar': false,
             'search': true,
-            'title': $('#parameter0').value
+            'title': document.getElementById("parameter0").value;
         });
       }
 
