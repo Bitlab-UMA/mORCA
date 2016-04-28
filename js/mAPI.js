893d39bd775dc5ae8311452eef87f958006384b9
juanfal <juan.falgueras@gmail.com>
2016-04-28 09:56:15 +0200

    selecting results

--- js/mAPI.js
+++ js/mAPI.js
@@ -385,10 +385,11 @@ function getFile(idfile, session, repoid) {
       // document.getElementById("mainresults").style.display = 'block';
       // alert(data);
       document.getElementById("mainresults").innerHTML = "<pre>" + cleanData(data) + "</pre>";
-      $("#mainresults").select(function() {
-        alert( "Handler for .select() called." );
-      });
-      $("#runrun").text("you can Copy results");
+      document.getElementById("mainresults").select();
+      // $("#mainresults").select(function() {
+      //   alert( "Handler for .select() called." );
+      // });
+      // $("#runrun").text("you can Copy results");
 
 
       // return data;  NO SIRVE, no hay return
