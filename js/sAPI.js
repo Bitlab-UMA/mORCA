71bd2ce78ab16b313e746665f3ddefc8875b3bc7
juanfal <juan.falgueras@gmail.com>
2016-06-30 12:26:12 +0200

    changing backg name in file reading sAPI.js:218

--- js/sAPI.js
+++ js/sAPI.js
@@ -211,7 +211,7 @@ function generateInterface(parameters) {
           '<div class="ui-block-a">' +
           '<div data-role="fieldcontain">' +
           '<p id="parameterhidden' + x + '" class="texthidden"></p>' +
-          '<input type="text" name="text-basic" id="parameter' + x + '" value="" data-inline="true" placeholder="Fetch a file from the cloud">' +
+          '<input type="text" name="text-basic" id="parameter' + x + '" value="" data-inline="true" placeholder="Fetch a file from mORCA previous outputs">' +
           //'<textarea name="text-basic" id="parameter'+x+'" value="" data-inline="true"> </textarea>' +
           '</div></div>' +
           '<div class="ui-block-b" style="padding-top:5px">' +
